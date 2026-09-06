d9533422af57eee46a1a188de8af3fee739f21a6
Himansu Mallik <dreamhimansu@gmail.com>
2026-09-06 16:29:35 +0530

    feat(auth): implement login endpoint with bcrypt verification and jwt generation

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req, res) =>{
     const {name, email, password} = req.body;
@@ -32,6 +33,45 @@ export const signup = async(req, res) =>{
         });
 
     } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+}
+
+export const signin = async(req, res) => {
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({message: "All fields are required"});
+    }
+
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message: "Invalid Credentials"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(401).json({message: "Invalid Credentials"});
+        }
+
+        const token = jwt.sign(
+            {id: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: process.env.JWT_EXPIRES_IN || '7d'}
+        )
+
+        return res.status(200).json({
+            message: `Login Successful`,
+            token, 
+            user: {
+                id: user._id,
+                name: user.name, 
+                email:user.email,
+            },
+        });
+
+    }catch(error){
         return res.status(500).json({message: error.message});
     }
 }
